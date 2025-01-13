f770b78fb5396cfa4b4c2c3a5c487bf01f2c87d4
hamza ali <engrhamza3409@gmail.com>
2025-01-14 01:21:16 +0500

    auth apis

---
+++ routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\API\PasswordResetController;
+use App\Http\Controllers\API\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// http://127.0.0.1:8000/api/register @ $ #
+
+route::post('/forget-password', [PasswordResetController::class, 'forgetPassword']);
+
+route::group(['middleware'=>'api'], function(){
+    route::post('/register', [UserController::class, 'register']);
+    route::post('/login', [UserController::class, 'login']);
+    route::get('/logout', [UserController::class, 'logout']);
+    route::get('/profile', [UserController::class, 'profile']);
+    route::post('/update-profile', [UserController::class, 'updateProfile']);
+    route::get('/send-verify-mail/{email}', [UserController::class, 'sendVerifyMail']);
+    route::get('/refresh-token', [UserController::class, 'refreshToken']);
+});
+
+
+
+
+
+
+
